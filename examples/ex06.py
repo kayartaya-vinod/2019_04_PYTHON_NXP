e40026a1e7068c6a5fc4b984aa558929210b577f
kayartaya-vinod <vinod@vinod.co>
2019-04-02 09:59:35 +0530

    🇮🇳

---
+++ examples/ex06.py
@@ -0,0 +1,34 @@
+'''
+This example demonstrates the use of "while" loop
+'''
+
+# the keyword "def" is used for creating a user defined function
+def multiplication_table(num):
+    i = 1
+    while i <= 10:
+        print("{} * {} --> {}".format(num, i, num*i))
+        i += 1
+
+def line(xyz):
+    print(xyz * 50)
+
+def factorial(num):
+    f = 1
+    while num>0:
+        f = f * num   # f *= num
+        num = num -1  # num -= 1
+        # print('f = {}, num = {}'.format(f, num))
+    return f
+
+def main():
+    num = int(input('Enter a number: '))
+    # multiplication_table(num)
+    
+    fact = factorial(num)
+    print('Factorial of %s is %s' % (num, fact))
+    # print('Factorial of %s is %s' % (num, factorial(num)))
+
+# print('in ex06.py, value of __name__ is', __name__)
+if __name__=='__main__':
+    main()
+    line('*')
---
+++ examples/t01.py
@@ -0,0 +1,2 @@
+import ex06
+print('in t01.py, value of __name__ is', __name__)
