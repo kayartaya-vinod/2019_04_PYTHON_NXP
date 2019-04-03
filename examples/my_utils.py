c3d2b5932f951a6e10e3797dd2b0a691bc04fddc
kayartaya-vinod <vinod@vinod.co>
2019-04-03 12:29:26 +0530

    🇮🇳

---
+++ examples/my_utils.py
@@ -0,0 +1,44 @@
+'''
+Some useful utility functions.
+'''
+import json
+
+def dirr(obj):
+    return [attr for attr in dir(obj) if attr[:1] != '_']
+
+def csv2json(filename):
+    list_of_people = []
+    with open(filename) as file:
+        header = file.readline().strip().split(',')
+        for line in file:
+            data = line.strip().split(',')
+            p1 = dict(zip(header, data))
+            list_of_people.append(p1)
+
+    # file is automatically closed here
+    dot_index = filename.index('.')
+    new_filename = filename[:dot_index] + '.json'
+    
+    with open(new_filename, 'w') as file:
+        json.dump(list_of_people, file, indent=3)
+
+def csv2json_v2(filename):
+    with open(filename) as file:
+        header = file.readline().strip().split(',')
+        list_of_people = [
+            dict(zip(header, line.strip().split(',')))
+            for line in file
+        ]
+        
+    dot_index = filename.index('.')
+    new_filename = filename[:dot_index] + '.json'
+    
+    with open(new_filename, 'w') as new_file:
+        json.dump(list_of_people, new_file, indent=3)
+
+
+def main():
+    csv2json_v2('people.csv')
+    print('JSON file has been generated!')
+
+if __name__=='__main__': main()
